06907b9e9ec89e7ef7de6dcf8a761d4eec957602
C_137 Rick <C_137 Rick@DESKTOP-6PEOR14>
2019-09-09 21:13:32 +0800

    client补充

--- 影像商城/ImageMall/ImageMall_Web/Controllers/HelperHttpClient.cs
+++ 影像商城/ImageMall/ImageMall_Web/Controllers/HelperHttpClient.cs
@@ -6,7 +6,7 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web;
 
-namespace HospitalScheds.Common
+namespace ImageMall_Web.Controllers
 {
     public static class HelperHttpClient
     {
